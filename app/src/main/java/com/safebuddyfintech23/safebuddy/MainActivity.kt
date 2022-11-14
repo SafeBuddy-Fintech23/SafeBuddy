d8e41b1c9b1a830b9fbb786663cf0af753cb1113
SamuelWakoli <swwakoli@gmail.com>
2022-11-14 19:47:47 +0300

    Created bottom navigation view for the home-screen.

--- app/src/main/java/com/safebuddyfintech23/safebuddy/MainActivity.kt
+++ app/src/main/java/com/safebuddyfintech23/safebuddy/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.safebuddyfintech23.safebuddy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.safebuddyfintech23.safebuddy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var toolbar: MaterialToolbar
+    private lateinit var navController: NavController
+    private lateinit var bottomNavView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        bottomNavView = binding.bottomNavView
+        val navHostFrag =
+            supportFragmentManager.findFragmentById(R.id.nav_host_frag) as NavHostFragment
+        navController = navHostFrag.navController
+
+
+        val topLevelDestinations = setOf(R.id.homeFragment, R.id.notificationsFragment, R.id.scanQrFragment, R.id.myProfileFragment, R.id.moreFragment)
+        val appBarConfiguration = AppBarConfiguration(topLevelDestinations)
+        toolbar = binding.toolbar
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+        bottomNavView.setupWithNavController(navController)
+
     }
 }
