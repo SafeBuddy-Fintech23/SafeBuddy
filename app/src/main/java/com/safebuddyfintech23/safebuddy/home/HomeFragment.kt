ce02062771098b3f4c8f7ee70a9f996526a8ec89
SamuelWakoli <swwakoli@gmail.com>
2022-11-22 02:55:30 +0300

    Developed the fragment_home UI that will show when the user's total due payment is $0

--- app/src/main/java/com/safebuddyfintech23/safebuddy/home/HomeFragment.kt
+++ app/src/main/java/com/safebuddyfintech23/safebuddy/home/HomeFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import com.safebuddyfintech23.safebuddy.R
 
 /**
@@ -13,11 +16,39 @@ import com.safebuddyfintech23.safebuddy.R
 
 class HomeFragment : Fragment() {
 
+    //lateinit my views
+    private lateinit var userName: TextView
+    private lateinit var dayORnightGreetings: TextView
+    private lateinit var userProfileImage: ImageView
+    private lateinit var duePayment: TextView
+    private lateinit var amountPaid: TextView
+    private lateinit var amountPaidProgressBar: ProgressBar
+    private lateinit var upcomingRecentTxt: TextView
+    private lateinit var emptyActivityHolderImg: ImageView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //init the views
+        userName = view.findViewById(R.id.txt_username_home)
+        dayORnightGreetings = view.findViewById(R.id.txt_daynight_greetings_home)
+        userProfileImage = view.findViewById(R.id.img_userprofile_home)
+        duePayment = view.findViewById(R.id.txt_due_amount_home)
+        amountPaid = view.findViewById(R.id.txt_amount_paid_home)
+        amountPaidProgressBar = view.findViewById(R.id.pgbr_amount_paid_home)
+        /*these last two view must be visible when the user has no upcoming or
+        recent installments. Since these views take huge space in the screen, I have
+        set their visibility to View.GONE in the layout file.
+        */
+        upcomingRecentTxt = view.findViewById(R.id.txt_upcoming_recent_home)
+        emptyActivityHolderImg = view.findViewById(R.id.img_empty_holder_home)
     }
 }
