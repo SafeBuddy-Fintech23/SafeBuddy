6f2fffc6186430b0d1272fcd0265f4162b0ecda4
SamuelWakoli <swwakoli@gmail.com>
2022-12-09 13:51:37 +0300

    Almost completed the CreateProfileFragment.kt that uses Firebase Database

--- app/src/main/java/com/safebuddyfintech23/safebuddy/CreateProfileFragment.kt
+++ app/src/main/java/com/safebuddyfintech23/safebuddy/CreateProfileFragment.kt
@@ -1,18 +1,201 @@
 package com.safebuddyfintech23.safebuddy
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.safebuddyfintech23.safebuddy.model.User
 
 class CreateProfileFragment : Fragment() {
+    companion object {
+        private const val USERS = "USERS"
+        private val CurrentUserName = FirebaseAuth.getInstance().currentUser?.displayName.toString()
+        private var PROFILE_PICS = "USER PROFILE PICTURES/$CurrentUserName"
+    }
+
+    //late init views
+    private lateinit var profileImgView: ImageView
+    private lateinit var setProfileTxt: TextView
+    private lateinit var firstNameEdtTxt: EditText
+    private lateinit var lastNameEdtTxt: EditText
+    private lateinit var bankIdEdtTxt: EditText
+    private lateinit var locationEdtTxt: EditText
+    private lateinit var btnSave: Button
+    private lateinit var dataSavedTxt: TextView
+    private lateinit var customerSupportTxt: TextView
+
+    private lateinit var imageUri: Uri
+
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_create_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //init views
+        profileImgView = view.findViewById(R.id.img_user_profile)
+        setProfileTxt = view.findViewById(R.id.txt_set_profile)
+        firstNameEdtTxt = view.findViewById(R.id.edit_txt_first_name)
+        lastNameEdtTxt = view.findViewById(R.id.edit_txt_last_name)
+        bankIdEdtTxt = view.findViewById(R.id.edit_txt_bank_id)
+        locationEdtTxt = view.findViewById(R.id.edit_txt_location)
+        btnSave = view.findViewById(R.id.btn_continue_create)
+        dataSavedTxt = view.findViewById(R.id.txt_notify_data_saved)
+        customerSupportTxt = view.findViewById(R.id.txt_customer_support)
+
+        dataSavedTxt.visibility = View.GONE
+        //Getting Profile Pic
+        // Click listeners on [profileImgView] and [setProfileTxt] to get user profile image.
+        //If one of them is clicked, [getProfileImg()]
+        profileImgView.setOnClickListener { getProfileImg() }
+        setProfileTxt.setOnClickListener { getProfileImg() }
+
+        // var for Getting text data
+        var inputFirstName: String?
+        var inputLastName: String?
+        var inputBankID: String?
+        var inputUserLocation: String?
+
+        //Saving User Data
+        btnSave.setOnClickListener {
+            //Request the user to wait while saving data
+            Toast.makeText(requireContext(), getString(R.string.plz_wait), Toast.LENGTH_LONG).show()
+
+            //Check if User Data is valid
+            //Raise errors where necessary if any field is empty
+            if (firstNameEdtTxt.text.isEmpty()) {
+                firstNameEdtTxt.error = getString(R.string.err_firstname)
+            } else if (lastNameEdtTxt.text.isEmpty()) {
+                lastNameEdtTxt.error = getString(R.string.err_lastname)
+            } else if (bankIdEdtTxt.text.isEmpty()) {
+                bankIdEdtTxt.error = getString(R.string.err_bank_id)
+            } else if (locationEdtTxt.text.isEmpty()) {
+                locationEdtTxt.error = getString(R.string.err_user_location)
+            } else {
+                //input data
+                inputFirstName = firstNameEdtTxt.text.toString()
+                inputLastName = lastNameEdtTxt.text.toString()
+                inputBankID = bankIdEdtTxt.text.toString()
+                inputUserLocation = locationEdtTxt.text.toString()
+
+                //then upload it
+                uploadUserData(
+                    requireContext(),
+                    imageUri,
+                    inputFirstName!!,
+                    inputLastName!!,
+                    inputBankID!!,
+                    inputUserLocation!!
+                )
+
+            }
+        }
+
+        customerSupportTxt.setOnClickListener {
+            //Create an intent to open web or send email
+
+            //this toast should be removed later
+            Toast.makeText(requireContext(), "Customer Support Unavailable", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+    //ToDo: Fix Deprecated
+    //Function to select image from device storage
+    private fun getProfileImg() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(intent, 24)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 24 && data != null && data.data != null) {
+            imageUri = data.data!!
+            profileImgView.setImageURI(imageUri)
+        }
+    }
+
+    //Uploads user data to Firebase Database
+    private fun uploadUserData(
+        context: Context,
+        uri: Uri,
+        firstName: String,
+        lastName: String,
+        bankID: String,
+        location: String,
+    ) { //if [CurrentUserName] is "null", set it to [firstName]+[lastName]
+        //this is because when user signs in using phone, [CurrentUserName] will be "null"
+        if (CurrentUserName == "null") PROFILE_PICS = firstName + lastName
+
+        // First, upload the image to Firebase Storage
+        val storageReference = FirebaseStorage.getInstance().getReference(PROFILE_PICS)
+        //store the image then get its downloadURL, using [.addOnSuccessListener]
+        storageReference.putFile(uri).addOnSuccessListener(requireActivity()) {
+
+            //get downloadURL from metadata,  then addOnSuccessListener to save the downloadURL
+            //to the database
+                taskSnapshot ->
+            taskSnapshot.metadata!!.reference!!.downloadUrl.addOnSuccessListener { downloadURL ->
+
+                //save input to the database
+                val user = User(
+                    downloadURL.toString(), firstName, lastName, bankID, location
+                )
+
+                //ref for the database
+                val database = FirebaseDatabase.getInstance().getReference(USERS)
+
+                //Create a node to the database for the user
+                val userNodeInDB = firstName + lastName
+
+                //now we input to the database
+                database.child(userNodeInDB).setValue(user).addOnSuccessListener {
+                    // After the data has been uploaded; additional activities to be done here
+
+                    // Notify the user that the profile has been created
+                    profileImgView.isClickable = false
+                    setProfileTxt.isClickable = false
+                    firstNameEdtTxt.isEnabled = false
+                    lastNameEdtTxt.isEnabled = false
+                    bankIdEdtTxt.isEnabled = false
+                    locationEdtTxt.isEnabled = false
+
+                    btnSave.setOnClickListener {
+                        Toast.makeText(
+                            context, getString(R.string.profile_saved), Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    dataSavedTxt.visibility = View.VISIBLE
+
+
+                    // ToDo: Change the views at the profile fragment, with reference to a shared pref*
+                }
+            }
+        }.addOnFailureListener {
+            //if the WHOLE process of uploading data fails, notify the user
+            Toast.makeText(
+                context, getString(R.string.profile_not_saved), Toast.LENGTH_SHORT
+            ).show()
+            dataSavedTxt.text = getString(R.string.profile_not_created)
+            dataSavedTxt.visibility = View.VISIBLE
+
+        }
     }
 }
---
+++ app/src/main/java/com/safebuddyfintech23/safebuddy/model/User.kt
@@ -0,0 +1,29 @@
+package com.safebuddyfintech23.safebuddy.model
+
+/**
+ * This will be used when uploading user data to the Firebase database.
+ */
+class User {
+    var profileUrl: String? = null
+    var firstName: String? = null
+    var lastName: String? = null
+    var bankID: String? = null
+    var location: String? = null
+
+    // Empty constructor to be used for serialization
+    constructor()
+
+    constructor(
+        profileUrl: String?,
+        firstName: String?,
+        lastName: String?,
+        bankID: String?,
+        location: String?
+    ) {
+        this.profileUrl = profileUrl
+        this.firstName = firstName
+        this.lastName = lastName
+        this.bankID = bankID
+        this.location = location
+    }
+}
